e312e9a4dae75d3869bd80dffcec6f0be57a1e7f
Jonathan Miller <jonathan@winepos.com>
2017-03-07 16:58:07 -0500

    Secured todo SDR repository

--- src/main/java/io/essolutions/todo/TodoRepository.java
+++ src/main/java/io/essolutions/todo/TodoRepository.java
@@ -1,17 +1,46 @@
 package io.essolutions.todo;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 	
-	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	@PreAuthorize("hasRole('ADMIN')")
+	@RestResource(exported=false)
+	public List<Todo> findAll();
+	
+	@PreAuthorize("#oauth2.hasScope('read') AND hasRole('ADMIN')")
 	@RestResource(exported=true)
 	public Page<Todo> findAll(Pageable pageable);
 	
+	@PreAuthorize("permitAll()")
+	@RestResource(exported=true)
+	@Query("SELECT todo from Todo todo where todo.secret = false")
+	public Page<Todo> findAllNonSecret(Pageable pageable);
+	
+	@PreAuthorize("(#oauth2.hasScope('read') AND hasPermission()) AND hasRole('USER')")
+	@RestResource(exported=true)
+	@Query("SELECT todo from Todo todo where todo.secret = true")
+	public Page<Todo> findAllSecret(Pageable pageable);
+	
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	@RestResource(exported=true)
+	@Override
+	public void delete(Todo entity);
+
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	@PostFilter("hasPermission()")
+	@RestResource(exported=true)
+	@Override
+	public <S extends Todo> S save(S entity);
+		
 }
