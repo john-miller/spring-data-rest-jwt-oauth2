333d82f364b492d2a61852e58efec0371523f331
Jonathan Miller <jonathan@winepos.com>
2017-03-07 16:52:37 -0500

    Added account domain object

---
+++ src/main/java/io/essolutions/account/Account.java
@@ -0,0 +1,113 @@
+package io.essolutions.account;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Account implements UserDetails  {
+ 
+	private static final long serialVersionUID = -2927133137069870011L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String username;
+
+	@JsonIgnore
+	private String password;
+	
+	@JsonIgnore
+	private boolean accountNonExpired;
+	
+	@JsonIgnore
+	private boolean accountNonLocked;
+	
+	@JsonIgnore
+	private boolean credentialsNonExpired;
+	
+	@JsonIgnore
+	private boolean enabled;
+
+	@JsonIgnore
+	@ManyToMany(fetch=FetchType.EAGER)
+	private Collection<Authority> authorities = new ArrayList<Authority>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAccountNonExpired() {
+		return accountNonExpired;
+	}
+
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public boolean isAccountNonLocked() {
+		return accountNonLocked;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		return credentialsNonExpired;
+	}
+
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Collection<Authority> authorities) {
+		this.authorities = authorities;
+	}
+		
+}
